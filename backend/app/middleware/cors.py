3cc47b4a8945fa6ba76c2f05310717786f0fc4de
Hattakorn Pongsawai <hattakorn49@gmail.com>
2026-04-30 00:10:13 +0700

    Update cors.py

--- backend/app/middleware/cors.py
+++ backend/app/middleware/cors.py
@@ -1,7 +1,7 @@
 from fastapi.middleware.cors import CORSMiddleware
 import os
 
-ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000", "https://ropa2077-frontend.vercel.app").split(",")
+ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000,https://ropa2077-frontend.vercel.app").split(",")
 
 def add_cors_middleware(app):
     app.add_middleware(
